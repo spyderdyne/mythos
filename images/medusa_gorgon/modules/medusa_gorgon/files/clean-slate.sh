1dd770b5df0037fc209e964ee5bd5adbce4e272f
Spyderdyne <spyderdyne@gmail.com>
2015-12-10 14:22:13 -0500

    added nuclear-option and clean-slate scripts for environment reset.  added sleep timers to keep from abusing the server wherever needed.

---
+++ images/medusa_gorgon/modules/medusa_gorgon/files/clean-slate.sh
@@ -0,0 +1,9 @@
+#1bin/bash
+
+#provides: clean-slate.sh
+
+# delete old host records
+/bin/rm -rf $mythos_home/seshat/clients/*
+
+# activate the nuclear option...
+/bin/mv $medusa_home/remote-scripts/.nuclear-option.sh $medusa_home/remote-scripts/nuclear-option.sh
