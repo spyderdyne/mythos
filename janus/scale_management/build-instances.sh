107fa46eb6c7c103d8a7006b04029ebc3515cfe3
James Scollard <spyderdyne@gmail.com>
2016-03-11 13:27:45 -0500

    added apache licenses

--- janus/scale_management/build-instances.sh
+++ janus/scale_management/build-instances.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Copyright 2016 Cisco Systems, Inc.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# author:  David Oyler
+# email:  doyler@cisco.com
+# source: https://code.launchpad.net/~openstack-tailgate/openstack-tailgate/mythos
+# source: https://github.com/spyderdyne/mythos/blob/master/janus/scale_management/build-instances.sh
+#
+# creates instances for scale testing
+
+
 # Default network layout
 
 ENVRC="/PATH/TO/RC/FILE"                            # path to your RC fileSUBNETADDR="192.168.50.0/24"
